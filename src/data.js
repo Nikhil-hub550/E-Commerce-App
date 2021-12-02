74a1c973c8e2323598eb9cb6bce24534d951692d
Nikhil <nikhil1889.cse18@chitkara.edu.in>
2021-12-02 21:25:31 +0530

    Add navbar, Add Slides

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/GMN4xLX/woman-png-favpng-P0t-Gs93teti-P7-Agpj6a0-Uhth-Z-t.jpg",
+        title: "SUMMER SALE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIAVLS",
+        bg:"f5fafd"    
+    },
+    {
+        id: 2,
+        img:"https://i.ibb.co/yFbZ7y4/winter.jpg",
+        title: "Winter SALE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIAVLS",
+        bg:"fcf1ed"    
+    },
+    {
+        id: 3,
+        img:"https://i.ibb.co/bW65Vpr/autumn.webp",
+        title: "Autumn SALE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIAVLS",
+        bg:"fbf0f4"    
+    },
+];
